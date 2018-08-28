d9f79a00138b898cb9ce4fd5bda6d68fd3fb9244
Molda <hbeygel@gmail.com>
2018-08-28 17:04:15 +0300

    * initial commit ---------------------------------------

---
+++ npm-instll.sh
@@ -0,0 +1,8 @@
+npm install --save axios && \\
+    npm install --save react-native-elements && \\
+    npm install --save react-native-vector-icons && \\
+    npm install --save react-navigation && \\
+    npm install --save react-redux && \\
+    npm install --save redux && \\
+    npm install --save redux-thunk && \\
+    npm i --save-dev jest
